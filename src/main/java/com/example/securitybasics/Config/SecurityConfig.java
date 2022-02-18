13cb2d0a9ee1a582035e74eba0b9c9ab1d75458b
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-02-18 15:27:40 +0300

    updating the security configurations

---
+++ src/main/java/com/example/securitybasics/Config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.securitybasics.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig implements WebMvcConfigurer {
+
+    @Configuration
+    protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter{
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication()
+                    .withUser("user1").password("user1").authorities("user")
+                    .and()
+                    .withUser("user2").password("user2").authorities("user");
+        }
+    }
+}
