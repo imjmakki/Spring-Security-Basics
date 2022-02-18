945a548ac4de2cd30b009acec5dd39574f36a89d
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-02-18 15:16:20 +0300

    spring security basics project

---
+++ src/main/java/com/example/securitybasics/SecurityBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitybasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityBasicsApplication.class, args);
+    }
+
+}
